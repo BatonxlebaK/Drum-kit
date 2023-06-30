25be74c7f89be6508d060c319ad65b363e3573c8
BatonxlebaK <125285391+BatonxlebaK@users.noreply.github.com>
2023-06-30 17:59:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.addEventListener("keydown", playSound)
 console.log(key);
 function playSound(event) {
-    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key-btn="${event.keyCode}"]`);
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
 
     if (!audio) {
         return;
     };
     audio.currentTime = 0;
-    // key.classList.add("playing");
+    key.classList.add("playing");
     audio.play();
     setTimeout(function () {
         key.classList.remove("playing");
